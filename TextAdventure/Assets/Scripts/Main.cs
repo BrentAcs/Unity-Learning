80d3ca92e95e4f9490c5d0fddbd4207e26aad22f
Brent Acs <brent.mann@gmail.com>
2023-05-06 18:15:46 -0400

    Start

---
+++ TextAdventure/Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//  Ref:  https://www.youtube.com/watch?v=xmR07iBW7zk
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
