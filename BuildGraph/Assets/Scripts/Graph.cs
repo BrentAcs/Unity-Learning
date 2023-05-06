64a5057300226d9b0dde6f6d80e0531069433b7a
Brent Acs <brent.mann@gmail.com>
2023-05-06 16:01:27 -0400

    added missing files

--- BuildGraph/Assets/Scripts/Graph.cs
+++ BuildGraph/Assets/Scripts/Graph.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+// Reference: https://catlikecoding.com/unity/tutorials/basics/building-a-graph/
 
 public class Graph : MonoBehaviour
 {
@@ -12,19 +11,42 @@ public class Graph : MonoBehaviour
    [Range(10,100)]
    private int resolution = 10;
 
+   private Transform[] points;
+
    private void Awake()
    {
       float step = 2f / resolution;
       var position = Vector3.zero;
       var scale = Vector3.one * step;
-      for (int i = 0; i < resolution; i++)
+      points = new Transform[resolution];
+      for (int i = 0; i < points.Length; i++)
       {
          Transform point = Instantiate(pointPrefab);
+         points[ i ] = point;
          position.x = (i + 0.5f) * step - 1f;
-         position.y = position.x * position.x;
          point.localPosition = position;
          point.localScale = scale;
          point.SetParent(transform, false);
+      }
+   }
+
+   // my silly idea, probably WAY wrong.
+   // private const int DelayCount = 25;
+   // private int currentDelay = 0;
+   
+   private void Update()
+   {
+      // if (currentDelay++ < DelayCount)
+      //    return;
+      // currentDelay = 0;
+      
+      var time = Time.time;
+      for (int i = 0; i < points.Length; i++)
+      {
+         var point = points[ i ];
+         var position = point.localPosition;
+         position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+         point.localPosition = position;
       }
    }
 }
--- Clock/Assets/Scripts/Clock.cs
+++ Clock/Assets/Scripts/Clock.cs
@@ -1,44 +1,50 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+// Reference: https://catlikecoding.com/unity/tutorials/basics/game-objects-and-scripts/
 public class Clock : MonoBehaviour
 {
-   [SerializeField]
-   private Transform hoursPivot;
-   
-   [SerializeField]
-   private Transform minutesPivot;
+    [SerializeField]
+    private Transform hoursPivot;
 
-   [SerializeField]
-   private Transform secondsPivot;
+    [SerializeField]
+    private Transform minutesPivot;
 
-   private const float HoursToDegrees = -30f;
-   private const float MinutesToDegrees = -6f;
-   private const float SecondsToDegrees = -6f;
-   
-   private void Awake()
-   {
-      UpdateClock();
-   }
+    [SerializeField]
+    private Transform secondsPivot;
 
-   // Start is called before the first frame update
-   void Start()
-   {
-   }
+    private const float HoursToDegrees = -30f;
 
-   // Update is called once per frame
-   void Update()
-   {
-      UpdateClock();
-   }
+    private const float MinutesToDegrees = -6f;
 
-   private void UpdateClock()
-   {
-      var time = DateTime.Now.TimeOfDay;
-      hoursPivot.localRotation = Quaternion.Euler(0f,0f,(float) (HoursToDegrees * time.TotalHours));
-      minutesPivot.localRotation = Quaternion.Euler(0f,0f,(float) (MinutesToDegrees * time.TotalMinutes));
-      secondsPivot.localRotation = Quaternion.Euler(0f,0f,(float) (SecondsToDegrees * time.TotalSeconds));
-   }
+    private const float SecondsToDegrees = -6f;
+
+    private void Awake()
+    {
+        UpdateClock();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateClock();
+    }
+
+    private void UpdateClock()
+    {
+        var time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation =
+            Quaternion.Euler(0f, 0f, (float)(HoursToDegrees * time.TotalHours));
+        minutesPivot.localRotation =
+            Quaternion
+                .Euler(0f, 0f, (float)(MinutesToDegrees * time.TotalMinutes));
+        secondsPivot.localRotation =
+            Quaternion
+                .Euler(0f, 0f, (float)(SecondsToDegrees * time.TotalSeconds));
+    }
 }
